66be758903c4c44caa7e3dce4a96940271658c29
Sulik <uraniummore@gmail.com>
2020-03-04 17:54:25 +0600

    Changed pages extensions so it's `jsx` or `tsx` now instead of `js`.

--- next.config.js
+++ next.config.js
@@ -2,4 +2,6 @@
 // Read more: https://github.com/zeit/next-plugins/tree/master/packages/next-css
 const withCSS = require("@zeit/next-css");
 
-module.exports = withCSS();
+module.exports = withCSS({
+    pageExtensions: ["jsx", "tsx"]
+});
