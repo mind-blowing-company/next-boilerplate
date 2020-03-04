b1673a65ed0bbb18d8ebb81325f65e2010c2dcd8
Sulik <uraniummore@gmail.com>
2020-03-04 18:28:00 +0600

    Added a test example with Enzyme usage.

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import {Provider} from "react-redux";
+import {mount} from "enzyme";
 
 import App from "../pages/App";
 import initStore from "../redux/store";
@@ -18,4 +19,14 @@ test("works with snapshot", () => {
     const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
+});
+
+test("works with Enzyme", () => {
+    const page = mount(
+        <Provider store={initStore()}>
+            <App i18n={{language: "en_US"}} t={key => key}/>
+        </Provider>
+    );
+
+    expect(page.find("button").text()).toEqual("switch-locale");
 });
